98abde196bb1b47478a6f0394f4069077b3e031b
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-01-05 12:03:15 -0300

    a

--- trab2_ed2_final/Alocacao-23/alocacao.c
+++ trab2_ed2_final/Alocacao-23/alocacao.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include "23.h"
 
-int initialize_memory_blocks(TreeNode23 **estrutura_memoria, int capacidade_maxima) {
+int initialize_memory_blocks(TreeNode23 **memory_structure, int max_capacity) {
     int current_state;
 
     do {
         printf("\n=== Configuração do Bloco Inicial ===\n");
         printf("Escolha o estado do infoBlock inicial:\n");
-        printf("  [%d] -> Disponível\n", ALLOCATED_STATUS_FREE);
-        printf("  [%d] -> Ocupado\n", STATUS_OCCUPIED);
+        printf("%d -> Disponível\n", ALLOCATED_STATUS_FREE);
+        printf("%d -> Ocupado\n", STATUS_OCCUPIED);
         printf("Digite sua escolha: ");
         scanf("%d", &current_state);
         while (getchar() != '\n'); 
@@ -23,10 +23,10 @@ int initialize_memory_blocks(TreeNode23 **estrutura_memoria, int capacidade_maxi
         printf("\n>>> Insira o endereço inicial do infoBlock: ");
         scanf("%d", &infoBlock.num_start);
         while (getchar() != '\n'); 
-        if (infoBlock.num_start < 0 || infoBlock.num_start >= capacidade_maxima) {
-            printf("\n[Erro] O endereço inicial deve estar entre 0 e %d.\n", capacidade_maxima - 1);
+        if (infoBlock.num_start < 0 || infoBlock.num_start >= max_capacity) {
+            printf("\n[Erro] O endereço inicial deve estar entre 0 e %d.\n", max_capacity - 1);
         }
-    } while (infoBlock.num_start < 0 || infoBlock.num_start >= capacidade_maxima);
+    } while (infoBlock.num_start < 0 || infoBlock.num_start >= max_capacity);
 
     int endereco_minimo = infoBlock.num_start;
 
@@ -37,16 +37,16 @@ int initialize_memory_blocks(TreeNode23 **estrutura_memoria, int capacidade_maxi
             printf("\n>>> Insira o endereço final do infoBlock (deve ser >= %d): ", infoBlock.num_start);
             scanf("%d", &infoBlock.num_end);
             while (getchar() != '\n'); 
-            if (infoBlock.num_end < infoBlock.num_start || infoBlock.num_end >= capacidade_maxima) {
-                printf("\n[Erro] O endereço final deve estar entre %d e %d.\n", infoBlock.num_start, capacidade_maxima - 1);
+            if (infoBlock.num_end < infoBlock.num_start || infoBlock.num_end >= max_capacity) {
+                printf("\n[Erro] O endereço final deve estar entre %d e %d.\n", infoBlock.num_start, max_capacity - 1);
             }
-        } while (infoBlock.num_end < infoBlock.num_start || infoBlock.num_end >= capacidade_maxima);
+        } while (infoBlock.num_end < infoBlock.num_start || infoBlock.num_end >= max_capacity);
 
-        TreeNode23_insert(estrutura_memoria, infoBlock);
+        TreeNode23_insert(memory_structure, infoBlock);
 
         infoBlock.num_start = infoBlock.num_end + 1;
         current_state = !current_state; 
-    } while (infoBlock.num_end < (capacidade_maxima - 1));
+    } while (infoBlock.num_end < (max_capacity - 1));
 
     printf("\n[Sucesso] Blocos inicializados com sucesso!\n");
     return endereco_minimo;
@@ -58,144 +58,144 @@ void merge_no(TreeNode23 **root, int *num_end, int limite, int valor_remover) {
     TreeNode23_remove(root, valor_remover);
 }
 
-TreeNode23 *find_smallest_block(TreeNode23 **estrutura, TreeNode23 *nodo, Info *info_dados, Info **menor_valor) {
-    TreeNode23 *smallest_block, *pai_temporario;
-    *menor_valor = NULL;
+TreeNode23 *find_smallest_block(TreeNode23 **treeRoot, TreeNode23 *nodo, Info *data_info, Info **minimum_value) {
+    TreeNode23 *smallest_block, *temporary_parent;
+    *minimum_value = NULL;
 
     if (isLeaf(*nodo)) {
-        if (nodo->info1.num_start != info_dados->num_start) {
+        if (nodo->info1.num_start != data_info->num_start) {
             smallest_block = nodo;
         } else {
-            smallest_block = Find_smallest_parent_node(*estrutura, info_dados->num_start);
+            smallest_block = Find_smallest_parent_node(*treeRoot, data_info->num_start);
         }
 
         if (smallest_block != NULL) {
-            if (smallest_block->n_infos == 2 && smallest_block->info2.num_start < info_dados->num_start) {
-                *menor_valor = &(smallest_block->info2);
+            if (smallest_block->n_infos == 2 && smallest_block->info2.num_start < data_info->num_start) {
+                *minimum_value = &(smallest_block->info2);
             } else {
-                *menor_valor = &(smallest_block->info1);
+                *minimum_value = &(smallest_block->info1);
             }
         }
-    } else if (nodo->info1.num_start == info_dados->num_start) {
-        smallest_block = Find_largest_child_node(nodo->left, &pai_temporario, menor_valor);
+    } else if (nodo->info1.num_start == data_info->num_start) {
+        smallest_block = Find_largest_child_node(nodo->left, &temporary_parent, minimum_value);
     } else {
-        smallest_block = Find_largest_child_node(nodo->center, &pai_temporario, menor_valor);
+        smallest_block = Find_largest_child_node(nodo->center, &temporary_parent, minimum_value);
     }
 
     return smallest_block;
 }
 
-TreeNode23 *find_largest_block(TreeNode23 **estrutura, TreeNode23 *nodo, Info *info_dados, Info **maior_valor) {
-    TreeNode23 *largest_block, *pai_temporario;
-    *maior_valor = NULL;
+TreeNode23 *find_largest_block(TreeNode23 **treeRoot, TreeNode23 *nodo, Info *data_info, Info **largest_value_pointer) {
+    TreeNode23 *largest_block, *temporary_parent;
+    *largest_value_pointer = NULL;
 
     if (isLeaf(*nodo)) {
-        if (nodo->n_infos == 2 && nodo->info1.num_start == info_dados->num_start) {
+        if (nodo->n_infos == 2 && nodo->info1.num_start == data_info->num_start) {
             largest_block = nodo;
         } else {
-            largest_block = Find_max_parent_node(*estrutura, info_dados->num_start);
+            largest_block = Find_max_parent_node(*treeRoot, data_info->num_start);
         }
 
         if (largest_block != NULL) {
-            if (largest_block->info1.num_start > info_dados->num_start) {
-                *maior_valor = &(largest_block->info1);
+            if (largest_block->info1.num_start > data_info->num_start) {
+                *largest_value_pointer = &(largest_block->info1);
             } else {
-                *maior_valor = &(largest_block->info2);
+                *largest_value_pointer = &(largest_block->info2);
             }
         }
     } else {
-        if (nodo->info1.num_start == info_dados->num_start) {
-            largest_block = Find_min_child_node(nodo->center, &pai_temporario);
+        if (nodo->info1.num_start == data_info->num_start) {
+            largest_block = Find_min_child_node(nodo->center, &temporary_parent);
         } else {
-            largest_block = Find_min_child_node(nodo->right, &pai_temporario);
+            largest_block = Find_min_child_node(nodo->right, &temporary_parent);
         }
 
         if (largest_block != NULL) {
-            *maior_valor = &(largest_block->info1);
+            *largest_value_pointer = &(largest_block->info1);
         }
     }
 
     return largest_block;
 }
 
-void adjust_node_in_tree(TreeNode23 **estrutura, TreeNode23 *nodo_atual, Info *info_atual, int tamanho) {
+void adjust_node_in_tree(TreeNode23 **treeRoot, TreeNode23 *current_node, Info *current_info, int size) {
     TreeNode23 *smallest_block;
     Info *smallest_data = NULL;
 
-    smallest_block = find_smallest_block(estrutura, nodo_atual, info_atual, &smallest_data);
+    smallest_block = find_smallest_block(treeRoot, current_node, current_info, &smallest_data);
 
-    if (tamanho < (info_atual->num_end - info_atual->num_start + 1)) {
+    if (size < (current_info->num_end - current_info->num_start + 1)) {
         if (smallest_block == NULL) {
             Info novo_dado;
-            novo_dado.num_start = info_atual->num_start;
-            novo_dado.num_end = info_atual->num_start + tamanho - 1;
-            novo_dado.status = !(info_atual->status);
+            novo_dado.num_start = current_info->num_start;
+            novo_dado.num_end = current_info->num_start + size - 1;
+            novo_dado.status = !(current_info->status);
 
-            info_atual->num_start += tamanho;
-            TreeNode23_insert(estrutura, novo_dado);
+            current_info->num_start += size;
+            TreeNode23_insert(treeRoot, novo_dado);
         } else {
-            smallest_data->num_end += tamanho;
-            info_atual->num_start += tamanho;
+            smallest_data->num_end += size;
+            current_info->num_start += size;
         }
     } else {
         TreeNode23 *largest_block;
         Info *largest_item = NULL;
 
-        largest_block = find_largest_block(estrutura, nodo_atual, info_atual, &largest_item);
+        largest_block = find_largest_block(treeRoot, current_node, current_info, &largest_item);
 
         if (smallest_block == NULL && largest_block == NULL) {
-            info_atual->status = !(info_atual->status);
+            current_info->status = !(current_info->status);
         } else {
             if (smallest_block == NULL) {
-                info_atual->status = !(info_atual->status);
-                merge_no(estrutura, &(info_atual->num_end), largest_item->num_end, largest_item->num_start);
+                current_info->status = !(current_info->status);
+                merge_no(treeRoot, &(current_info->num_end), largest_item->num_end, largest_item->num_start);
             } else if (largest_block == NULL) {
-                merge_no(estrutura, &(smallest_data->num_end), info_atual->num_end, info_atual->num_start);
+                merge_no(treeRoot, &(smallest_data->num_end), current_info->num_end, current_info->num_start);
             } else {
                 int remover_endereco = largest_item->num_start;
-                merge_no(estrutura, &(smallest_data->num_end), largest_item->num_end, info_atual->num_start);
-                TreeNode23_remove(estrutura, remover_endereco);
+                merge_no(treeRoot, &(smallest_data->num_end), largest_item->num_end, current_info->num_start);
+                TreeNode23_remove(treeRoot, remover_endereco);
             }
         }
     }
 }
 
-TreeNode23 *search_node(TreeNode23 **arvore, int quant, int status, Info **info_escolhido) {
+TreeNode23 *search_node(TreeNode23 **treeRoot, int quantity, int status, Info **selected_info) {
     TreeNode23 *node;
-    if (*arvore != NULL) {
-        node = search_node(&((*arvore)->left), quant, status, info_escolhido);
+    if (*treeRoot != NULL) {
+        node = search_node(&((*treeRoot)->left), quantity, status, selected_info);
 
-        if (*info_escolhido == NULL) {
-            if ((*arvore)->info1.status == status &&
-                ((*arvore)->info1.num_end - (*arvore)->info1.num_start + 1) >= quant) {
-                *info_escolhido = &((*arvore)->info1);
-                node = *arvore;
+        if (*selected_info == NULL) {
+            if ((*treeRoot)->info1.status == status &&
+                ((*treeRoot)->info1.num_end - (*treeRoot)->info1.num_start + 1) >= quantity) {
+                *selected_info = &((*treeRoot)->info1);
+                node = *treeRoot;
             } else {
-                node = search_node(&((*arvore)->center), quant, status, info_escolhido);
-                if ((*arvore)->n_infos == 2) {
-                    if ((*arvore)->info2.status == status &&
-                        ((*arvore)->info2.num_end - (*arvore)->info2.num_start + 1) >= quant) {
-                        *info_escolhido = &((*arvore)->info2);
-                        node = *arvore;
-                    } else if (*info_escolhido == NULL)
-                        node = search_node(&((*arvore)->right), quant, status, info_escolhido);
+                node = search_node(&((*treeRoot)->center), quantity, status, selected_info);
+                if ((*treeRoot)->n_infos == 2) {
+                    if ((*treeRoot)->info2.status == status &&
+                        ((*treeRoot)->info2.num_end - (*treeRoot)->info2.num_start + 1) >= quantity) {
+                        *selected_info = &((*treeRoot)->info2);
+                        node = *treeRoot;
+                    } else if (*selected_info == NULL)
+                        node = search_node(&((*treeRoot)->right), quantity, status, selected_info);
                 }
             }
         }
     } else
-        *info_escolhido = NULL;
+        *selected_info = NULL;
 
     return node;
 }
 
-int manage_memory_block(TreeNode23 **estrutura, int tamanho, int estado_atual) {
+int manage_memory_block(TreeNode23 **treeRoot, int size, int estado_atual) {
     Info *target_block = NULL;
-    TreeNode23 *nodo_alvo = search_node(estrutura, tamanho, estado_atual, &target_block);
+    TreeNode23 *nodo_alvo = search_node(treeRoot, size, estado_atual, &target_block);
 
     if (target_block != NULL) {
         printf("\nDetalhes do infoBlock selecionado:\n");
         showNodeInfo23(*target_block);
-        adjust_node_in_tree(estrutura, nodo_alvo, target_block, tamanho);
+        adjust_node_in_tree(treeRoot, nodo_alvo, target_block, size);
     } else {
         printf("\nNão foram encontrados blocos disponíveis com o critério especificado.\n");
     }
