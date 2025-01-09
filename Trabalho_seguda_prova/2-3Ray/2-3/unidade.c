818b08061b3bfbfbe4da3c9a6d879eb160123373
Alisson Rodrigo <alissorodrigo098@gmail.com>
2025-01-09 18:04:47 -0300

    as

---
+++ Trabalho_seguda_prova/2-3Ray/2-3/unidade.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "unidade.h"
+
+int remover_lista_encadeada_unidade(Unit **lista, int nome) {
+    int confirmacao = 0;
+
+    if (*lista) {
+        if ((*lista)->unitValue == nome) {
+            Unit *aux = *lista;
+            *lista = (*lista)->nextNode;
+            // Não liberar `unitValue` se não foi alocado dinamicamente
+            free(aux);
+            confirmacao = 1;
+        } else {
+            confirmacao = remover_lista_encadeada_unidade(&(*lista)->nextNode, nome);
+        }
+    }
+
+    return confirmacao;
+}
+
+
+Unit *find_unit(Unit *unitList, int unitValue) {
+    Unit *aux = unitList;
+
+    while (aux != NULL && aux->unitValue != unitValue) {
+        aux = aux->nextNode;
+    }
+
+    return aux;
+}
+
+void print_units(Unit *unitList) {
+    Unit *aux = unitList;
+    while (aux != NULL) {
+        printf("Unidade: %d\n", aux->unitValue);
+        aux = aux->nextNode;
+    }
+}
+
+
+Unit *create_unit(int unitValue) {
+    Unit *nova_unidade = (Unit *)malloc(sizeof(Unit));
+    if (nova_unidade != NULL) {
+        nova_unidade->unitValue = unitValue;
+        nova_unidade->nextNode = NULL;
+    }
+    return nova_unidade;
+}
+
+
+Unit *insert_unit_sorted(Unit *unitList, Unit *new_unit) {
+    Unit *head = unitList;  
+    Unit *previousUnit = NULL;
+    Unit *currentUnit = unitList;
+
+    if (unitList == NULL) {
+      
+        printf("Adicionando unidade %d como a primeira na lista.\n", new_unit->unitValue);
+        new_unit->nextNode = NULL;
+        head = new_unit;
+    } else {
+ 
+        while (currentUnit != NULL && currentUnit->unitValue < new_unit->unitValue) {
+            previousUnit = currentUnit;
+            currentUnit = currentUnit->nextNode;
+        }
+
+       
+        if (currentUnit != NULL && currentUnit->unitValue == new_unit->unitValue) {
+            printf("Unidade %d já existe na lista. Ignorando.\n", new_unit->unitValue);
+            free(new_unit);
+        } else if (previousUnit == NULL) {
+          
+            new_unit->nextNode = unitList;
+            head = new_unit;
+            printf("Unidade %d adicionada no início da lista.\n", new_unit->unitValue);
+        } else {
+       
+            previousUnit->nextNode = new_unit;
+            new_unit->nextNode = currentUnit;
+            printf("Unidade %d adicionada na lista.\n", new_unit->unitValue);
+        }
+    }
+
+    return head;  
+}
+
+
+
+
+void free_list(Unit *unitList) {
+    Unit *currentUnit = unitList;
+    while (currentUnit != NULL) {
+        Unit *temp = currentUnit;
+        currentUnit = currentUnit->nextNode;
+        free(temp);
+    }
+}
